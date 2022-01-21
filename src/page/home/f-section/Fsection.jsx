bb64a9560100a9d25725710b13febf340fcfe261
saddam-maratouq <sadam.sadam.sadam@hotmail.com>
2022-01-21 20:33:01 +0200

    finsh first section but still media query

--- src/page/home/f-section/Fsection.jsx
+++ src/page/home/f-section/Fsection.jsx
@@ -1,20 +1,38 @@
-import React from 'react'
+import React from "react";
 
-import './FSec.css' 
+import "./FSec.css"; 
 
+import robot from '../../../../images/robo.png'
 
 
 const Fsection = () => {
-
-
   return (
     <section className="info">
-    <div>
-        hello world
-    </div>
-   </section>
+      <div className="main-info">
+        <p> Your smart partner for </p> 
 
-  )
-}
+        <br /> <br /> 
+        <h1>
+        
+          Digital Transformation Through <br /> AI Product Development & <br /> Storytelling
+        </h1>
+        <br /> <br /> 
+        <p>
+          In an age of information overload, <br />  we help our clients make sense of
+          it all.
+        </p> 
 
-export default Fsection 
+     <div className="get-start">
+         <button> GET STARTED   </button>
+     </div>
+      </div>
+
+       <div className="robot">
+           <img src={ robot } alt="smart robot" />
+       </div>
+     
+    </section> 
+  );
+};
+
+export default Fsection;
